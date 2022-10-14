b29468883f035358eeff5872096ef818e57e09e5
Engin Bolat <engi.bolat@gmail.com>
2022-10-14 16:35:20 +0300

    BottomNavBar texts added git status

--- lib/constants/app_text.dart
+++ lib/constants/app_text.dart
@@ -1,3 +1,9 @@
 class AppText{
   static String appName = "MovieApp";
 }
+
+class BottomNavBarPageText{
+  static String moviesText = "Movies";
+  static String tvSeriesText = "Tv Series";
+  static String profileText = "Profile";
+}
