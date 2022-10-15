1a84e2a76da0d32d3a20a61fb2622624eb0f6d6d
Engin Bolat <engi.bolat@gmail.com>
2022-10-15 14:06:34 +0300

    Actor and Actress List added

--- lib/view/movie_details_page.dart
+++ lib/view/movie_details_page.dart
@@ -1,36 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app_bloc/services/json_services/json_services.dart';
+import 'package:movie_app_bloc/constants/app_radius.dart';
 import 'package:movie_app_bloc/widget/spacer_widget.dart';
 
 import '../constants/app_api.dart';
+import '../constants/app_padding.dart';
+import '../model/movie_actor_model.dart';
 import '../services/cubit/movie_cubit.dart';
 
-class MovieDetailsPage extends StatefulWidget {
+class MovieDetailsPage extends StatelessWidget {
   const MovieDetailsPage({super.key, required this.movieId});
   final String movieId;
 
   @override
-  State<MovieDetailsPage> createState() => _MovieDetailsPageState();
-}
-
-class _MovieDetailsPageState extends State<MovieDetailsPage> {
-  Map<String, dynamic>? modelList;
-
-  Future<void> transferData() async {
-    modelList = await JsonServices().fetchMovieDetails(widget.movieId);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    transferData();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => MovieCubit()..getMovieDetails(widget.movieId),
+      create: (context) {
+        return MovieCubit()..getMovieDetails(movieId);
+      },
       child: Scaffold(
         appBar: AppBar(),
         body: BlocConsumer<MovieCubit, MovieState>(
@@ -40,7 +27,7 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
               return _movieLoading();
             } else if (state is GetMovieDataState) {
               var data = state.movieDetailsModel;
-              return _movieDetailsWidget(context, data);
+              return _movieDetailsWidget(context, data, state.movieActorModel);
             } else {
               return _error();
             }
@@ -50,19 +37,20 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
     );
   }
 
-  SingleChildScrollView _movieDetailsWidget(
-      BuildContext context, Map<String, dynamic>? data) {
+  SingleChildScrollView _movieDetailsWidget(BuildContext context,
+      Map<String, dynamic>? data, List<MovieActorModel>? movieActorList) {
     return SingleChildScrollView(
       child: Center(
         child: Column(
           children: [
             ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(MovieDetailsPageRadius.posterValue),
+                bottomRight:
+                    Radius.circular(MovieDetailsPageRadius.posterValue),
               ),
               child: SizedBox(
-                  height: 400,
+                  height: MediaQuery.of(context).size.height * 0.6,
                   width: MediaQuery.of(context).size.width,
                   child: Image.network(
                     fit: BoxFit.fill,
@@ -70,12 +58,14 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                   )),
             ),
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 24.0),
+              padding: EdgeInsets.symmetric(
+                  horizontal: MoviePagePaddig.minimumValue,
+                  vertical: MoviePagePaddig.normalValue),
               child: Column(
                 children: [
                   Text(
                     data?["title"],
+                    textAlign: TextAlign.center,
                     style: Theme.of(context)
                         .textTheme
                         .headline4!
@@ -101,6 +91,43 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                         .headline6!
                         .copyWith(fontWeight: FontWeight.normal),
                   ),
+                  SpacerWidget(pageContext: context, coefficient: 0.05),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: movieActorList?.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding:
+                                EdgeInsets.all(MoviePagePaddig.minimumValue),
+                            child: Column(
+                              children: [
+                                Text(
+                                  movieActorList?[index].name ?? "",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5!
+                                      .copyWith(
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.black54),
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.4,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.5,
+                                  child: Image.network(
+                                    ApiConst.posterPath +
+                                        (movieActorList?[index].profilePath ??
+                                            ""),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                  )
                 ],
               ),
             ),
@@ -112,20 +139,20 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
 
   ClipRRect _buildGenresBox(BuildContext context, Map<String, dynamic>? data) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
+      borderRadius: BorderRadius.circular(MovieDetailsPageRadius.normalValue),
       child: Container(
         height: MediaQuery.of(context).size.height * .135,
         width: double.infinity,
         color: Theme.of(context).primaryColor,
         child: Padding(
-          padding: const EdgeInsets.all(24.0),
+          padding: EdgeInsets.all(MoviePagePaddig.normalValue),
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    data?["genres"][0]["name"] ?? "No Genres",
+                    data?["genres"][0]["name"].toString() ?? "No Genres",
                     style: Theme.of(context).textTheme.headline6!.copyWith(
                           color: Colors.white,
                         ),
