f91b658419cd0aa4908861daeb8a2e5f5c5396ec
Engin Bolat <engi.bolat@gmail.com>
2022-10-16 12:45:08 +0300

    statusbar become a transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:movie_app_bloc/services/cubit/movie_cubit.dart';
 import 'package:movie_app_bloc/view/bottom_navbar_page.dart';
 
@@ -7,6 +9,8 @@ import 'constants/app_text.dart';
 import 'constants/app_theme.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -15,6 +19,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
     return BlocProvider(
       create: (context) => MovieCubit(),
       child: MaterialApp(
